5f4a2921e0af94d42c9b01356caa5178f7714438
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-08-26 17:21:03 +0300

    Update Admin.java

--- src/main/java/openevents/Admin.java
+++ src/main/java/openevents/Admin.java
@@ -2,11 +2,15 @@ package openevents;
 
 import openevents.utils.BaseUserEntity;
 import openevents.utils.Person;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 
 
 @Entity
+@DynamicInsert
+@DynamicUpdate
 @Table(name = "admin")
 public class Admin extends BaseUserEntity {
 
