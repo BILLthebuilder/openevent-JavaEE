4e6376e7aa15240209f2b9f19818e200cc8e5381
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-08-26 17:21:28 +0300

    Update Organizer.java

--- src/main/java/openevents/Organizer.java
+++ src/main/java/openevents/Organizer.java
@@ -1,10 +1,14 @@
 package openevents;
 import openevents.utils.BaseUserEntity;
 import openevents.utils.Person;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 
 @Entity
+@DynamicInsert
+@DynamicUpdate
 @Table(name = "organizers")
 public class Organizer extends BaseUserEntity {
     @Embedded
