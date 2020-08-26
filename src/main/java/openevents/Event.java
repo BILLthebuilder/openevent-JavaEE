d1f24e9e04483ca0293badb4649ae9c16a808ee5
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-08-26 17:21:23 +0300

    Update Event.java

--- src/main/java/openevents/Event.java
+++ src/main/java/openevents/Event.java
@@ -1,10 +1,15 @@
 package openevents;
 
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@DynamicInsert
+@DynamicUpdate
 @Table(name = "events")
 public class Event {
 
@@ -15,16 +20,14 @@ public class Event {
     @Column
     private String address;
 
+    @ManyToOne()
+    private Organizer organizer;
+
+    @OneToMany(mappedBy = "event")
+    private List<Attendee> attendees = new ArrayList<>();
+
     @Column
-    private int organizerId;
-
-    @Column(name = "attendee_id's")
-    @ElementCollection
-    private List<Integer> attendeeIds = new ArrayList<Integer>();
-
-    @Column
-    private boolean approved;
-
+    private Boolean approved;
 
     public int getId() {
         return id;
@@ -42,27 +45,29 @@ public class Event {
         this.address = address;
     }
 
-    public int getOrganizerId() {
-        return organizerId;
+    public Organizer getOrganizer() {
+        return organizer;
     }
 
-    public void setOrganizerId(int organizerId) {
-        this.organizerId = organizerId;
+    public void setOrganizer(Organizer organizer) {
+        this.organizer = organizer;
     }
 
-    public List<Integer> getAttendeeIds() {
-        return attendeeIds;
+    public List<Attendee> getAttendees() {
+        return attendees;
     }
 
-    public void setAttendeeIds(List<Integer> attendeeIds) {
-        this.attendeeIds = attendeeIds;
+    public void setAttendees(List<Attendee> attendees) {
+        this.attendees = attendees;
     }
 
     public boolean isApproved() {
         return approved;
     }
 
-    public void setApproved(boolean approved) {
+    public void setApproved(Boolean approved) {
         this.approved = approved;
     }
+
+
 }
