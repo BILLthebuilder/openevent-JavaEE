7496ae3ffc14bc37f396ffb666e775d28be4ffe6
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-08-27 16:55:26 +0300

    add many to many to attendees

--- src/main/java/openevents/Attendee.java
+++ src/main/java/openevents/Attendee.java
@@ -4,24 +4,33 @@ import openevents.utils.BaseUserEntity;
 import openevents.utils.Person;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
-import org.hibernate.annotations.Type;
+
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @DynamicInsert
 @DynamicUpdate
 @Table(name = "attendees")
 public class Attendee extends BaseUserEntity {
-
     @Embedded
     Person attendee;
 
     @Column(name = "rsvp",nullable = false)
     private Boolean RSVP;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Event event;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "event_attendance",
+            joinColumns = { @JoinColumn(name = "attendee_id") },
+            inverseJoinColumns = { @JoinColumn(name = "event_id") }
+    )
+    private Set<Event> events = new HashSet<>();
 
     public Person getAttendee() {
         return attendee;
@@ -37,6 +46,13 @@ public class Attendee extends BaseUserEntity {
 
     public void setRSVP(Boolean RSVP) {
         this.RSVP = RSVP;
+    }
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
     }
 
 }
