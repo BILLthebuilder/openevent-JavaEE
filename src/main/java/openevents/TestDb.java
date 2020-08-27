13822d41a49bafe7f827237cc175a0c01a6473f9
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-08-27 17:00:24 +0300

    Update TestDb.java

--- src/main/java/openevents/TestDb.java
+++ src/main/java/openevents/TestDb.java
@@ -5,50 +5,58 @@ import openevents.utils.Person;
 import org.hibernate.*;
 
 public class TestDb {
-    public static void createAttendees(){
-        Session session = HibernateHelper.getSessionFactory().getCurrentSession();
+    public static void main(String args[]){
+        //adminTests();
+        //createOrganizer();
+        //savingAttendeeUsingEvent();
+        savingEventsUsingOrganizer();
 
-        Transaction tx = session.getTransaction();
-        tx.begin();
-       Attendee attendee = new Attendee();
-       attendee.setAttendee(new Person());
 
-       attendee.getAttendee().setName("Bill");
-       attendee.getAttendee().setEmail("bill@email.com");
-       attendee.getAttendee().setPhoneNumber(734049589);
-       attendee.setRSVP(true);
-
-       session.save(attendee);
-
-        tx.commit();
     }
-    public static void createOrganizer(){
+    public static void savingEventsUsingOrganizer(){
         Session session = HibernateHelper.getSessionFactory().getCurrentSession();
 
         Transaction tx = session.getTransaction();
         tx.begin();
-        Organizer organizer = new Organizer();
+       Organizer organizer = new Organizer();
         organizer.setOrganizer(new Person());
-
         organizer.getOrganizer().setName("Bill");
         organizer.getOrganizer().setEmail("bill@email.com");
-        organizer.getOrganizer().setPhoneNumber(734049589);
+        organizer.getOrganizer().setPhoneNumber(700000009);
+
+
+        for (int idx = 0; idx<10; idx++) {
+            Event event = new Event();
+            event.setAddress("Nairobi");
+            event.setApproved(true);
+
+            organizer.addEvent(event);
+        }
+
         session.save(organizer);
 
         tx.commit();
-
     }
-    public static void eventTest(){
+    public static void savingAttendeeUsingEvent(){
         Session session = HibernateHelper.getSessionFactory().getCurrentSession();
 
         Transaction tx = session.getTransaction();
         tx.begin();
         Event event = new Event();
-        Organizer organizer = new Organizer();
-        //int organizerId = (Integer)organizer.getId();
         event.setAddress("Nairobi");
         event.setApproved(true);
-        event.setOrganizer(organizer);
+
+        for (int idx = 0; idx<10; idx++) {
+            Attendee attendee = new Attendee();
+            attendee.setAttendee(new Person());
+
+            attendee.setEvent(event);
+            attendee.getAttendee().setName("Bill");
+            attendee.getAttendee().setEmail("bill@email.com");
+            attendee.getAttendee().setPhoneNumber(700000009);
+            attendee.setRSVP(true);
+            event.addAttendee(attendee);
+        }
 
         session.save(event);
 
@@ -63,19 +71,12 @@ public class TestDb {
        Admin admin = new Admin();
        admin.setAdmin(new Person());
 
-       admin.getAdmin().setName("Person");
+       admin.getAdmin().setName("admin");
        admin.getAdmin().setEmail("admin@mail.com");
 
        session.save(admin);
 
        tx.commit();
    }
-    public static void main(String args[]){
-      //adminTests();
-        //createOrganizer();
-        //eventTest();
-        createAttendees();
 
-
-    }
 }
