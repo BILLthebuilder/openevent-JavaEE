08cff321e1ef405c9be6c9e208d6596b2de0935d
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-08-27 17:00:10 +0300

    add oneTomany to organizer

--- src/main/java/openevents/Organizer.java
+++ src/main/java/openevents/Organizer.java
@@ -5,6 +5,8 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @DynamicInsert
@@ -14,11 +16,27 @@ public class Organizer extends BaseUserEntity {
     @Embedded
     Person organizer;
 
+    @OneToMany(mappedBy = "organizer",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Event> events = new ArrayList<>();
+
     public Person getOrganizer() {
         return organizer;
     }
 
     public void setOrganizer(Person organizer) {
         this.organizer = organizer;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
+    public void addEvent(Event event){
+        event.setOrganizer(this);
+        this.events.add(event);
     }
 }
