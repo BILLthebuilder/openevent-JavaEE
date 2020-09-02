4c3196167b800fb30b7c3cb130c78e2b48327347
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-09-02 18:57:38 +0300

    add stuff to event action

--- src/main/java/openevents/actions/events/EventAction.java
+++ src/main/java/openevents/actions/events/EventAction.java
@@ -2,11 +2,12 @@ package openevents.actions.events;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.hibernate.*;
 
@@ -18,17 +19,34 @@ public class EventAction extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        String address = request.getParameter("event_address");
-        String approvedParam = request.getParameter("event_approval");
-        Boolean approved = Boolean.valueOf(approvedParam);
+        String tittle = request.getParameter("event_title");
+        String organizer = request.getParameter("event_organizer");
+        String tags = request.getParameter("event_tags");
+        String type = request.getParameter("event_type");
+        String category = request.getParameter("event_category");
+        String location = request.getParameter("event_location");
+        String startDate = request.getParameter("event_start_date");
+        String endDate = request.getParameter("event_end_date");
+
         
         Session session = HibernateHelper.getSessionFactory().getCurrentSession();
         Transaction tx = session.getTransaction();
         try {
             tx.begin();
             Event event = new Event();
-            event.setAddress(address);
-            event.setApproved(approved);
+            event.setLocation(location);
+            event.setOrganizerName(organizer);
+            event.setEventType(type);
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            String startDateInString = startDate;
+            String endDateInString = endDate;
+            Date startDate_ = simpleDateFormat.parse(startDateInString);
+            Date endDate_ = simpleDateFormat.parse(endDateInString);
+            event.setEventStartDateAndTime(startDate_);
+            event.setEventEndDateAndTime(endDate_);
+            event.setEventCategory(category);
+            event.setEventTags(tags);
+            event.setTitle(tittle);
             session.save(event);
             response.getWriter().println("Data saved successfully!!");
             tx.commit();
