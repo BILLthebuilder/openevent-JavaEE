e54698c1fbb5ae91f6ae705733edd24477a21bff
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-09-03 06:40:36 +0300

    create the user bean

---
+++ src/main/java/openevents/beans/UserBean.java
@@ -0,0 +1,63 @@
+package openevents.beans;
+
+import openevents.utils.BaseUserEntity;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "users")
+public class UserBean extends BaseUserEntity{
+
+@NotNull
+@Column(name = "first_name")
+private String firstName;
+
+@NotNull
+@Column(name = "last_name")
+private String lastName;
+
+@NotNull
+@Column(name ="email", unique = true, nullable = false)
+private String email;
+
+@NotNull
+@Column(name = "password")
+private String password;
+
+public String getFirstName() {
+    return firstName;
+}
+
+public void setFirstName(String firstName) {
+    this.firstName = firstName;
+}
+
+public String getLastName() {
+    return lastName;
+}
+
+public void setLastName(String lastName) {
+    this.lastName = lastName;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
+}
+
+public String getPassword() {
+    return password;
+}
+
+public void setPassword(String password) {
+    this.password = password;
+}
+}
